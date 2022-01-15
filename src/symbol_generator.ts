c8b146c1e9cdb9cd9ec2d008e3257ba85e0a2a0a
Joseph Pasque <pasquej@wustl.edu>
2022-01-15 13:46:17 -0600

    starting rewrite to ts

---
+++ src/symbol_generator.ts
@@ -0,0 +1,38 @@
+import fs from "fs";
+import path from "path";
+import {logger} from "./logger";
+
+const symbolsPath = path.join(__dirname,"../.symbols");
+
+const exchanges = ["nasdaq", "nyse"];
+
+const dirPresent = async (): Promise<boolean> => {
+    try {
+        await fs.promises.access(symbolsPath);
+        return true;
+    }
+    catch(e) {
+        return false;
+    }
+}
+
+const fullyBuiltExchangePath = (exchange: string) => path.join(symbolsPath, `${exchange}-formatted.json`);
+
+const exchangeFilesPresent = async (): Promise<boolean> => {
+    let allSuccess = true;
+    for (let exchange of exchanges) {
+        try {
+            await fs.promises.stat(fullyBuiltExchangePath(exchange));
+            logger.info(`✅ exchange ${exchange} present and ready in current symbol store`);
+        }
+        catch(e) {
+            logger.info(`❌ exchange ${exchange} missing from current symbol store`);
+            allSuccess = false;
+        }
+    }
+    return allSuccess;
+}
+
+export const generateIfMissing = () => {
+
+}
